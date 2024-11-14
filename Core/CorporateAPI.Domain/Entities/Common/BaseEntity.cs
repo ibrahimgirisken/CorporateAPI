b6c618c77fcf32443c5611a994f7a28158669191
ibrahim Girişken <ibrahimgirisken@hotmail.com>
2024-11-14 10:14:42 +0300

    Add project files.

---
+++ Core/CorporateAPI.Domain/Entities/Common/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorporateAPI.Domain.Entities.Common
+{
+    public class BaseEntity
+    {
+        public Guid MyProperty { get; set; } = new Guid();
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
---
+++ Infrastracture/CoreporateAPI.Persistence/Contexts/CorporateAPIDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreporateAPI.Persistence.Contexts
+{
+    public class CorporateAPIDbContext : DbContext
+    {
+        public CorporateAPIDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
