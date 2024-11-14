c8ce80e945e74ec6501c209b0618f79ddd6135ec
ibrahim Girişken <ibrahimgirisken@hotmail.com>
2024-11-14 17:56:35 +0300

    2024-11-14-17-56

---
+++ Core/CorporateAPI.Application/Repositories/Menu/IMenuReadRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorporateAPI.Application.Repositories.Menu
+{
+    public interface IMenuReadRepository:IReadRepository<Domain.Entities.MenuEntity.Menu>
+    {
+    }
+}
---
+++ Infrastracture/CoreporateAPI.Persistence/Repositories/Menu/MenuReadRepository.cs
@@ -0,0 +1,13 @@
+﻿using CorporateAPI.Application.Repositories.Menu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreporateAPI.Persistence.Repositories.Menu
+{
+    public class MenuReadRepository:ReadRepository<IMenuReadRepository>
+    {
+    }
+}
