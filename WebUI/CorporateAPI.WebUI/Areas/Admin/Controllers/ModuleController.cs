dee969d5a531d6413895d21b5af236cc5165fa5d
ibrahim Girişken <ibrahimgirisken@hotmail.com>
2024-12-19 17:52:42 +0300

    2024-12-19-17-52

---
+++ WebUI/CorporateAPI.WebUI/Areas/Admin/Controllers/ModuleController.cs
@@ -0,0 +1,39 @@
+﻿using CorporateAPI.WebUI.DTOs.Module;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CorporateAPI.WebUI.Areas.Admin.Controllers
+{
+    public class ModuleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> DeleteModule()
+        {
+            return null;
+        }
+
+        public IActionResult CreateModule()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateModule(ModuleDTO moduleDTO)
+        {
+            return null;
+        }
+
+        public IActionResult UpdateModule()
+        {
+            return null;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateModule(UpdateModuleDTO updateModuleDTO)
+        {
+            return null;
+        }
+    }
+}
---
+++ WebUI/CorporateAPI.WebUI/Areas/Admin/Controllers/PageController.cs
@@ -0,0 +1,54 @@
+﻿using CorporateAPI.WebUI.DTOs.Page;
+using CorporateAPI.WebUI.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CorporateAPI.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("[area]/[controller]/[action]/{id?}")]
+    public class PageController() : Controller
+    {
+        private readonly HttpClient _client= HttpClientInstance.CreateClient();
+        public async Task<IActionResult> Index()
+        {
+            var response = await _client.GetFromJsonAsync<ResultPagesDTO>("Pages");
+
+            if (response == null || response.Pages == null)
+            {
+                return View(new List<GetPageDTO>()); // Boş bir liste döner
+            }
+
+            return View(response.Pages);
+        }
+
+        public async Task<IActionResult> DeletePage(int id)
+        {
+            await _client.DeleteAsync("Pages/{id}");
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpGet]
+        public IActionResult CreatePage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreatePage(PageDTO pageDTO)
+        {
+            await _client.PostAsJsonAsync("Pages", pageDTO);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> UpdatePage(int id)
+        {
+            PageDTO response= await _client.GetFromJsonAsync<PageDTO>($"Pages/{id}");
+            return View(response);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdatePage(UpdatePageDTO updatePageDTO)
+        {
+            await _client.PutAsJsonAsync("Pages", updatePageDTO);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
