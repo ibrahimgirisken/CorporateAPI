1df542a34331487e4f5eeffeef88f15ec0b8fc6a
ibrahim Girişken <ibrahimgirisken@hotmail.com>
2025-01-21 12:37:58 +0300

    2025-01-21-12-37

---
+++ Core/CorporateAPI.Application/Features/Queries/Category/GetByIdCategory/GetByIdCategoryQueryRequest.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorporateAPI.Application.Features.Queries.Category.GetByIdCategory
+{
+    public class GetByIdCategoryQueryRequest:IRequest<GetByIdCategoryQueryResponse>
+    {
+    }
+}
