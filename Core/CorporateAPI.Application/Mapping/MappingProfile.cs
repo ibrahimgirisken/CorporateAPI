838af0c59d97f89a75f40403292da24ef2e31c77
ibrahim Girişken <ibrahimgirisken@hotmail.com>
2025-01-22 08:49:55 +0300

    2025-01-22-08-49

--- Core/CorporateAPI.Application/Mapping/MappingProfile.cs
+++ Core/CorporateAPI.Application/Mapping/MappingProfile.cs
@@ -1,12 +1,17 @@
 ﻿using AutoMapper;
 using CorporateAPI.Application.DTOs.Banner;
+using CorporateAPI.Application.DTOs.Brand;
+using CorporateAPI.Application.DTOs.Category;
 using CorporateAPI.Application.DTOs.Home;
 using CorporateAPI.Application.DTOs.Lang;
 using CorporateAPI.Application.DTOs.Menu;
 using CorporateAPI.Application.DTOs.Module;
 using CorporateAPI.Application.DTOs.Page;
+using CorporateAPI.Application.DTOs.Product;
 using CorporateAPI.Application.Features.Commands.Banner.CreateBanner;
 using CorporateAPI.Application.Features.Commands.Banner.UpdateBanner;
+using CorporateAPI.Application.Features.Commands.Brand.CreateBrand;
+using CorporateAPI.Application.Features.Commands.Category.CreateCategory;
 using CorporateAPI.Application.Features.Commands.Home.CreateHome;
 using CorporateAPI.Application.Features.Commands.Home.UpdateHome;
 using CorporateAPI.Application.Features.Commands.Lang.CreateLang;
@@ -16,11 +21,16 @@ using CorporateAPI.Application.Features.Commands.Menu.UpdateMenu;
 using CorporateAPI.Application.Features.Commands.Module.CreateModule;
 using CorporateAPI.Application.Features.Commands.Page.CreatePage;
 using CorporateAPI.Application.Features.Commands.Page.UpdatePage;
+using CorporateAPI.Application.Features.Commands.Product.CreateProduct;
+using CorporateAPI.Application.Features.Commands.Product.UpdateProduct;
 using CorporateAPI.Domain.Entities;
 using CorporateAPI.Domain.Entities.Banner;
+using CorporateAPI.Domain.Entities.Brand;
+using CorporateAPI.Domain.Entities.Category;
 using CorporateAPI.Domain.Entities.Home;
 using CorporateAPI.Domain.Entities.Menu;
 using CorporateAPI.Domain.Entities.Module;
+using CorporateAPI.Domain.Entities.Product;
 
 namespace CorporateAPI.Application.Mapping
 {
@@ -58,6 +68,19 @@ namespace CorporateAPI.Application.Mapping
             CreateMap<Lang,CreateLangCommandRequest>().ReverseMap();
             CreateMap<Lang,UpdateLangCommandRequest>().ReverseMap();
 
+            CreateMap<Product,ResultProductDTO>().ReverseMap();
+            CreateMap<Product,CreateProductCommandResponse>().ReverseMap();
+            CreateMap<Product,UpdateProductCommandResponse>().ReverseMap();
+            CreateMap<ProductTranslation, ProductTranslationDTO>().ReverseMap();
+
+            CreateMap<Category,ResultCategoryDTO>().ReverseMap();
+            CreateMap<Category,CreatecategoryCommandResponse>().ReverseMap();
+            CreateMap<Category,UpdateProductCommandResponse>().ReverseMap();
+            CreateMap<CategoryTranslation, CategoryTranslationDTO>().ReverseMap();
+
+            CreateMap<Brand,ResultBrandDTO>().ReverseMap();
+            CreateMap<Brand,CreateBrandCommandResponse>().ReverseMap();
+            CreateMap<Brand,UpdateBrandDTO>().ReverseMap();
 
         }
     }
