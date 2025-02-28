8ce2718dcbe0b578299ef148697c8827e844ea1e
ibrahim Girişken <ibrahimgirisken@hotmail.com>
2025-02-28 14:50:38 +0300

    2025-02-28-14-50

---
+++ Infrastracture/CoreporateAPI.Persistence/Services/RoleService.cs
@@ -0,0 +1,31 @@
+﻿using CorporateAPI.Application.Abstractions.Services;
+using CorporateAPI.Domain.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreporateAPI.Persistence.Services
+{
+    public class RoleService : IRoleService
+    {
+        readonly RoleManager<AppRole> roleManager;
+
+        public RoleService(RoleManager<AppRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+
+        public Task CreateRole(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteRole(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
