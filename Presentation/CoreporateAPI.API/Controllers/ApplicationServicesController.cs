4ef439b89ec7c2de9560c6c77d7ce72679da038d
ibrahim Girişken <ibrahimgirisken@hotmail.com>
2025-08-07 09:06:35 +0300

    2025-08-07-09-06

--- Presentation/CoreporateAPI.API/Controllers/ApplicationServicesController.cs
+++ Presentation/CoreporateAPI.API/Controllers/ApplicationServicesController.cs
@@ -8,7 +8,7 @@ namespace CoreporateAPI.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    //[Authorize(AuthenticationSchemes = "Admin")]
+    [Authorize(AuthenticationSchemes = "Admin")]
     public class ApplicationServicesController : ControllerBase
     {
         readonly IApplicationService _applicationService;
